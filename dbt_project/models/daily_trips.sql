14fd80ea7c16cdb6fa9aba51c2a3e3c642c9a308
Data-drone <bpl.law@gmail.com>
2021-07-01 00:38:16 +1000

    taking out delta worked though I think it is thrift acting up

--- dbt_project/models/daily_trips.sql
+++ dbt_project/models/daily_trips.sql
@@ -1,5 +1,5 @@
 select
     to_date(pickup_datetime) as `date`,
     count(*) as `num_trips`  
-from processed.nyc_taxi_dataset
+from processed.nyc_taxi_dataset_processed_parquet
 group by to_date(pickup_datetime)
